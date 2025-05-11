2f940ea62e216aa60a85a89887d6ae46e332148b
정형진 <88423849+KeMezz@users.noreply.github.com>
2025-05-11 20:10:39 +0900

    클라이언트 생성 시 알아서 jwt 토큰 참조하도록 개선

--- libs/apollo-client.ts
+++ libs/apollo-client.ts
@@ -4,16 +4,21 @@ import {
   ApolloClient,
   InMemoryCache,
 } from "@apollo/client-integration-nextjs";
+import getSession from "./session";
 
-export const getClient = (token?: string) =>
-  registerApolloClient(() => {
+export const getClient = async () => {
+  const session = await getSession();
+  const jwt = session.jwt;
+
+  return registerApolloClient(() => {
     return new ApolloClient({
       cache: new InMemoryCache(),
       link: new HttpLink({
         uri: process.env.NEXT_PUBLIC_GRAPHQL_API,
         headers: {
-          authorization: token ? `Bearer ${token}` : "",
+          authorization: jwt ? `Bearer ${jwt}` : "",
         },
       }),
     });
-  });
+  }).getClient();
+};
