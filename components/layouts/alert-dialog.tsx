183fde8ba813b3bb0dbf65e4989b5fd243b1cf9a
정형진 <88423849+KeMezz@users.noreply.github.com>
2025-05-21 23:05:20 +0900

    경고 다이얼로그, 토스트에 애니메이션 적용

--- components/layouts/alert-dialog.tsx
+++ components/layouts/alert-dialog.tsx
@@ -4,6 +4,30 @@ import * as AlertDialog from "@radix-ui/react-alert-dialog";
 import { alertAtom } from "@/libs/atoms";
 import { useAtom } from "jotai";
 import { useRouter } from "next/navigation";
+import { AnimatePresence, motion, Variants } from "framer-motion";
+
+const overlayVariants = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1 },
+  exit: { opacity: 0 },
+};
+
+const contentVariants: Variants = {
+  hidden: { opacity: 0, y: 60 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.2,
+      ease: "easeInOut",
+    },
+  },
+  exit: {
+    opacity: 0,
+    scale: 0.95,
+    transition: { duration: 0.2, ease: "easeInOut" },
+  },
+};
 
 export default function GlobalAlertDialog() {
   const router = useRouter();
@@ -17,7 +41,7 @@ export default function GlobalAlertDialog() {
       closeBtnAction,
       extraBtnText,
       extraBtnAction,
-      extraBtnColor,
+      extraBtnColor = "neutral",
       extraBtnLoading,
       relogin,
     },
@@ -56,31 +80,69 @@ export default function GlobalAlertDialog() {
 
   return (
     <AlertDialog.Root open={visible} onOpenChange={closeDialog}>
-      <AlertDialog.Portal>
-        <AlertDialog.Overlay className="fixed inset-0 z-50 bg-black/50" />
-        <AlertDialog.Content className="flex flex-col gap-2 fixed z-50 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white dark:bg-neutral-900 rounded-md p-6 shadow-md min-w-[340px] sm:min-w-[380px]">
-          <AlertDialog.Title className="text-lg font-semibold">
-            {title}
-          </AlertDialog.Title>
-          <AlertDialog.Description>
-            {description ? description : descriptionElement}
-          </AlertDialog.Description>
-          <div className="flex justify-end gap-3 mt-2">
-            <AlertDialog.Cancel
-              onClick={closeDialog}
-              className="cursor-pointer bg-neutral-500 text-white px-4 py-2 rounded-md hover:bg-neutral-600"
+      <AnimatePresence>
+        {visible && (
+          <AlertDialog.Portal forceMount>
+            <motion.div
+              className="fixed inset-0 z-50 bg-black/50"
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+              variants={overlayVariants}
             >
-              {closeBtn ? "취소" : null}
-            </AlertDialog.Cancel>
-            <AlertDialog.Action
-              onClick={onExtraBtnClick}
-              className="cursor-pointer bg-violet-500 text-white px-4 py-2 rounded-md hover:bg-violet-600"
+              <AlertDialog.Overlay className="fixed inset-0" />
+            </motion.div>
+
+            <motion.div
+              className="fixed z-50 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white dark:bg-neutral-900 rounded-md p-6 shadow-md min-w-[340px] sm:min-w-[380px]"
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+              variants={contentVariants}
             >
-              {extraBtnText}
-            </AlertDialog.Action>
-          </div>
-        </AlertDialog.Content>
-      </AlertDialog.Portal>
+              <AlertDialog.Content className="flex flex-col gap-4 h-full">
+                <AlertDialog.Title className="text-lg font-semibold">
+                  {title}
+                </AlertDialog.Title>
+                <AlertDialog.Description>
+                  {description ? description : descriptionElement}
+                </AlertDialog.Description>
+                <div className="flex justify-end gap-3 mt-2">
+                  <AlertDialog.Cancel
+                    onClick={closeDialog}
+                    className="cursor-pointer bg-neutral-500 text-white px-4 py-2 rounded-md hover:bg-neutral-600"
+                  >
+                    {closeBtn ? "취소" : null}
+                  </AlertDialog.Cancel>
+                  {relogin ? (
+                    <AlertDialog.Action
+                      onClick={pushToLogin}
+                      className={`cursor-pointer ${buttonColor(
+                        extraBtnColor
+                      )} px-4 py-2 rounded-md ${
+                        extraBtnLoading ? "animate-pulse" : ""
+                      }`}
+                    >
+                      {extraBtnText}
+                    </AlertDialog.Action>
+                  ) : (
+                    <AlertDialog.Action
+                      onClick={onExtraBtnClick}
+                      className={`cursor-pointer ${buttonColor(
+                        extraBtnColor
+                      )} px-4 py-2 rounded-md ${
+                        extraBtnLoading ? "animate-pulse" : ""
+                      }`}
+                    >
+                      {extraBtnText}
+                    </AlertDialog.Action>
+                  )}
+                </div>
+              </AlertDialog.Content>
+            </motion.div>
+          </AlertDialog.Portal>
+        )}
+      </AnimatePresence>
     </AlertDialog.Root>
   );
 }
--- components/layouts/toast.tsx
+++ components/layouts/toast.tsx
@@ -4,7 +4,37 @@ import { toastAtom } from "@/libs/atoms";
 import { useAtom } from "jotai";
 import * as Toast from "@radix-ui/react-toast";
 import { useEffect, useRef } from "react";
-import { InformationCircleIcon } from "@heroicons/react/24/outline";
+import {
+  ArrowTurnDownLeftIcon,
+  InformationCircleIcon,
+} from "@heroicons/react/24/outline";
+import { AnimatePresence, motion, Variants } from "framer-motion";
+
+const toastVariants: Variants = {
+  hidden: {
+    opacity: 0,
+    y: 50,
+    scale: 0.95,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    scale: 1,
+    transition: {
+      duration: 0.2,
+      ease: "easeOut",
+    },
+  },
+  exit: {
+    opacity: 0,
+    y: 10,
+    scale: 0.95,
+    transition: {
+      duration: 0.1,
+      ease: "easeIn",
+    },
+  },
+};
 
 export default function GlobalToast() {
   const [
@@ -21,29 +51,51 @@ export default function GlobalToast() {
       timerRef.current = window.setTimeout(() => {
         setToast({ visible: false, isError: false });
       }, duration);
+
+      return () => {
+        clearTimeout(timerRef.current);
+      };
     }
   }, [visible, setToast, duration]);
 
   return (
-    <Toast.Provider duration={duration}>
-      <Toast.Root
-        className={`flex flex-col justify-center gap-x-4 rounded-md px-4 py-3 shadow-md transition-all duration-300 ease-out transform data-[state=open]:translate-y-0 data-[state=open]:opacity-100 data-[state=closed]:translate-y-5 data-[state=closed]:opacity-0 ${
-          isError
-            ? "bg-rose-400 dark:bg-rose-900 text-white"
-            : "bg-neutral-900 text-white dark:bg-neutral-50 dark:text-neutral-900"
-        }`}
-        open={visible}
-      >
-        <Toast.Title className="flex items-center gap-2 font-semibold">
-          {isError ? <InformationCircleIcon className="size-5" /> : null}
-          {title}
-        </Toast.Title>
-        {description ? (
-          <Toast.Description className="text-xs mt-2">
-            {description}
-          </Toast.Description>
-        ) : null}
-      </Toast.Root>
+    <Toast.Provider duration={duration} swipeDirection="down">
+      <AnimatePresence>
+        {visible && (
+          <Toast.Root asChild forceMount open={visible}>
+            <motion.div
+              className={`flex flex-col justify-center gap-x-4 rounded-md px-4 py-3 shadow-md ${
+                isError
+                  ? "bg-rose-400 dark:bg-rose-900 text-white"
+                  : "bg-neutral-900 text-white dark:bg-neutral-50 dark:text-neutral-900"
+              }`}
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+              variants={toastVariants}
+            >
+              <Toast.Title className="flex items-center gap-2 font-semibold">
+                {isError ? <InformationCircleIcon className="size-5" /> : null}
+                {title}
+              </Toast.Title>
+              {description ? (
+                <Toast.Description className="text-xs mt-2">
+                  {description}
+                </Toast.Description>
+              ) : null}
+              {undoAction ? (
+                <Toast.Action
+                  altText="Undo"
+                  onClick={undoAction}
+                  className="text-xs mt-2"
+                >
+                  <ArrowTurnDownLeftIcon className="size-4" />
+                </Toast.Action>
+              ) : null}
+            </motion.div>
+          </Toast.Root>
+        )}
+      </AnimatePresence>
       <Toast.Viewport className="fixed bottom-24 sm:bottom-12 left-1/2 sm:left-auto sm:right-0 -translate-x-1/2 sm:translate-x-0 z-50 m-0 flex w-[390px] max-w-[100vw] list-none flex-col gap-2.5 p-6 outline-none" />
     </Toast.Provider>
   );
