0026ad989f442f21c5f34401abb0b1d7b5f51215
정형진 <88423849+KeMezz@users.noreply.github.com>
2025-06-26 00:23:59 +0900

    feat: 환경 변수 파일 및 스크립트 추가
    - .env.dev 및 .env.canary 파일을 .gitignore에 추가
    - APP_ENV에 따라 환경 변수를 로드하는 로직 추가
    - 개발 및 빌드 스크립트에 canary 및 prod 환경 추가
    - package-lock.json 파일 삭제

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,17 @@
 import type { NextConfig } from "next";
 
+const envFile =
+  process.env.APP_ENV === "canary"
+    ? ".env.canary"
+    : process.env.APP_ENV === "prod"
+    ? ".env"
+    : ".env.dev";
+
+require("dotenv").config({
+  path: envFile,
+  override: true,
+});
+
 const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
