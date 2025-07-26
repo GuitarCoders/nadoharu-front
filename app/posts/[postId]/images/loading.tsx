869f1904243f974fe7856ef46e96ce0582bc8e5e
정형진 <88423849+KeMezz@users.noreply.github.com>
2025-07-26 21:55:31 +0900

    feat: 게시글 이미지 클릭 시 로딩 컴포넌트 추가

---
+++ app/posts/[postId]/images/loading.tsx
@@ -0,0 +1,7 @@
+export default function PostImagesLoading() {
+  return (
+    <div className="fixed inset-0 bg-black z-50 flex items-center justify-center">
+      <div className="w-8 h-8 border-2 border-white border-t-transparent rounded-full animate-spin" />
+    </div>
+  );
+}
